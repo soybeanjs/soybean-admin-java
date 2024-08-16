f99c5297958d1d5424b1e72c1a725488ddcb4dd2
coding <luby6220@163.com>
2024-08-16 22:41:42 +0800

    优化包名

--- soybean-admin-portal/src/main/java/com/soybean/portal/ctrl/SysCtrl.java
+++ soybean-admin-portal/src/main/java/com/soybean/portal/ctrl/SysCtrl.java
@@ -1,6 +1,10 @@
 package com.soybean.portal.ctrl;
 
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @description:
@@ -9,10 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Copyright：
  */
 @RequestMapping("/sys/")
+@RestController
 public class SysCtrl {
 
-    @RequestMapping("/login")
-    public void login(){
-
+    // 会话登录接口
+    // 登录接口
+    @RequestMapping("doLogin")
+    public SaResult doLogin() {
+        // 第1步，先登录上
+        StpUtil.login(10001);
+        // 第2步，获取 Token  相关参数
+        SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+        // 第3步，返回给前端
+        return SaResult.data(tokenInfo);
     }
+
+
 }
--- soybean-admin-start/src/main/java/com/soybean/Application.java
+++ soybean-admin-start/src/main/java/com/soybean/Application.java
@@ -1,4 +1,4 @@
-package com.soybean.start;
+package com.soybean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
